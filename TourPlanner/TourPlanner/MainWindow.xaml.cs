f8e842ab1386ea4f3500b4956045b63217dc635e
your name <if20b089@technikum-wien.at>
2022-02-25 17:42:18 +0100

    Add basic project architecture and started UI Wireframe

--- TourPlanner/TourPlanner/MainWindow.xaml.cs
+++ TourPlanner/TourPlanner/MainWindow.xaml.cs
@@ -18,9 +18,8 @@ namespace TourPlanner
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
+    public partial class MainWindow : Window {
+	    public MainWindow()
         {
             InitializeComponent();
         }

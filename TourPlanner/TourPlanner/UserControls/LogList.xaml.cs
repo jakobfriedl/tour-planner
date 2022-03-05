f88ee198123b505d67cca6e63f6e1de4d31dc4ea
your name <if20b089@technikum-wien.at>
2022-03-05 12:47:32 +0100

    Split up MainWindow into User Controls

---
+++ TourPlanner/TourPlanner/UserControls/LogList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LogList.xaml
+    /// </summary>
+    public partial class LogList : UserControl
+    {
+        public LogList()
+        {
+            InitializeComponent();
+        }
+    }
+}

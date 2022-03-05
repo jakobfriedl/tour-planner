e6fa226864e2a00adf6871aa95307baa6a8bc3f6
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2022-03-05 16:58:49 +0100

    Add models and BaseViewModel-class

---
+++ TourPlanner/TourPlanner/ViewModels/TourListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.Models;
+using TourPlanner.ViewModels.Abstract;
+
+namespace TourPlanner.ViewModels
+{
+    internal class TourListViewModel : BaseViewModel {
+	    
+    }
+}
--- docs/TIME.md
+++ docs/TIME.md
@@ -3,4 +3,4 @@
 | Date | Time | Name | Feature |
 |---|---|---|---|
 |25.02.2022|3h|Jakob|Project Setup and UI Wireframe|
-|||||
+|05.03.2022|3h|Jakob|Project Structure and Views|

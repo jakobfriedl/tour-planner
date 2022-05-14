859d2efd18bf7d02602acc3bd81282141d16da73
Phil <71327300+Philscs@users.noreply.github.com>
2022-05-14 18:25:25 +0200

    finished Report creation
    Finished with Summarized and Tour Report creation

--- TourPlanner/TourPlanner.BusinessLayer/SummarizeReportGeneration.cs
+++ TourPlanner/TourPlanner.BusinessLayer/SummarizeReportGeneration.cs
@@ -9,6 +9,7 @@ using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using System.Windows;
+using TourPlanner.DataAccessLayer.SQL;
 
 namespace TourPlanner.BusinessLayer
 {
@@ -18,6 +19,7 @@ namespace TourPlanner.BusinessLayer
         private ObservableCollection<Tour>? Tours;
         public void SummarizeReportGenerator(ObservableCollection<Tour> tours, ObservableCollection<Log> logs)
         {
+            StatDAO stats = new StatDAO(new Database());
             Logs = logs;
             Tours = tours;
             var ReportPath = "Resources/reports/";
@@ -43,26 +45,26 @@ namespace TourPlanner.BusinessLayer
                                 table.ColumnsDefinition(columns =>
                                 {
                                     columns.RelativeColumn(15);
-                                    columns.RelativeColumn(18);
-                                    columns.RelativeColumn(13);
-                                    columns.RelativeColumn(13);
-                                    columns.RelativeColumn(8);
+                                    columns.RelativeColumn(16);
+                                    columns.RelativeColumn(12);
+                                    columns.RelativeColumn(12);
+                                    columns.RelativeColumn(12);
                                 });
                                 table.Header(header =>
                                 {
+                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).Text("Tour");
                                     header.Cell().BorderBottom(2).BorderColor(Colors.Black).Text("Route");
-                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).Text("Destination");
+                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).AlignRight().Text("avg. Rating");
+                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).AlignRight().Text("avg. Difficulty");
                                     header.Cell().BorderBottom(2).BorderColor(Colors.Black).AlignRight().Text("avg. Duration");
-                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).AlignRight().Text("avg. Distance");
-                                    header.Cell().BorderBottom(2).BorderColor(Colors.Black).AlignRight().Text("Rating");
                                 });
                                 foreach (Tour t in Tours)
                                 {
                                     table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).Text(t.Name).FontSize(12);
                                     table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).Text($"{t.Start} - {t.Destination}").FontSize(12);
-                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(calculateAverageDuration(t.Id)).FontSize(12);
-                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(Math.Round(t.Distance, 2) + " km").FontSize(12);
-                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(calculateAverageRating(t.Id)).FontSize(12);
+                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(stats.GetAvgRating(t.Id)).FontSize(12);
+                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(stats.GetAvgDifficulty(t.Id)).FontSize(12);
+                                    table.Cell().BorderBottom(1).BorderColor(Colors.Grey.Lighten2).AlignRight().Text(TimeSpan.FromSeconds(stats.GetAvgDuration(t.Id))).FontSize(12);
                                 }
                             });
                         });
@@ -76,55 +78,6 @@ namespace TourPlanner.BusinessLayer
                 });
             });
             document.GeneratePdf($"{ReportPath}summarized.pdf");
-        }
-
-        public string calculateAverageDuration(int id)
-        {
-            int avgDuration = 0;
-            var logs = 0;
-            foreach (var log in Logs)
-            {
-                if (log.TourId == id)
-                {
-                    avgDuration += log.TotalTime;
-                    logs++;
-                }
-            }
-            if (logs > 0)
-            {
-                avgDuration = avgDuration / logs;
-                return FormatTime(avgDuration).ToString("G").Split(',')[0];
-            }
-            Tour? tour = Tours.Single(i => i.Id == id);
-            return tour.DisplayTime.ToString();
-        }
-        public int calculateAverageRating(int id)
-        {
-            int avgRating = 0;
-            var logs = 0;
-            foreach (var log in Logs)
-            {
-                if (log.TourId == id)
-                {
-                    avgRating += log.Rating;
-                    logs++;
-                }
-            }
-            if (logs > 0)
-            {
-                avgRating = avgRating / logs;
-                return avgRating;
-            }
-            Tour tour = Tours.Single(i => i.Id == id);
-            return (int)tour.ChildFriendliness;
-        }
-
-        private TimeSpan FormatTime(int time)
-        {
-            var s = time % 60;
-            var m = (time / 60) % 60;
-            var h = (time / 3600);
-            return new TimeSpan(h, m, s);
         }
     }
 }
